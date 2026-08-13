2a13f0b0ed8d7ff3a603435a8620c92d3536904b
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-13 14:47:05 +0300

    refactor(layouts): extract dashboard navbar into AppNavbar

---
+++ app/components/AppNavbar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const router = useRouter()
+const { breadcrumbs, backTo } = usePageHeader()
+
+function goBack() {
+  if (backTo.value) {
+    router.push(backTo.value)
+  } else {
+    router.back()
+  }
+}
+</script>
+
+<template>
+  <UDashboardNavbar>
+    <template #leading>
+      <!--            <UDashboardSidebarCollapse /> -->
+      <UButton
+        v-if="backTo"
+        icon="i-lucide-arrow-left"
+        color="neutral"
+        variant="ghost"
+        size="sm"
+        class="ms-1"
+        @click="goBack"
+      />
+    </template>
+
+    <template #title>
+      <UBreadcrumb
+        v-if="breadcrumbs.length"
+        :items="breadcrumbs"
+      />
+    </template>
+
+    <template #right>
+      <slot name="right">
+        <UColorModeButton />
+      </slot>
+    </template>
+  </UDashboardNavbar>
+</template>
--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-const router = useRouter()
-const { breadcrumbs, backTo } = usePageHeader()
-
-function goBack() {
-  if (backTo.value) {
-    router.push(backTo.value)
-  } else {
-    router.back()
-  }
-}
-</script>
-
 <template>
   <UDashboardGroup
     unit="rem"
@@ -20,31 +7,7 @@ function goBack() {
 
     <UDashboardPanel>
       <template #header>
-        <UDashboardNavbar>
-          <template #leading>
-            <!--            <UDashboardSidebarCollapse /> -->
-            <UButton
-              v-if="backTo"
-              icon="i-lucide-arrow-left"
-              color="neutral"
-              variant="ghost"
-              size="sm"
-              class="ms-1"
-              @click="goBack"
-            />
-          </template>
-
-          <template #title>
-            <UBreadcrumb
-              v-if="breadcrumbs.length"
-              :items="breadcrumbs"
-            />
-          </template>
-
-          <template #right>
-            <UColorModeButton />
-          </template>
-        </UDashboardNavbar>
+        <AppNavbar />
       </template>
 
       <template #body>
