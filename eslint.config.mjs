3640a9cb207b016992c6645f875038339904c847
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-20 18:37:27 +0300

    fix(eslint): ignore openapi-typescript schema

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   {
+    ignores: ['app/api/schema.d.ts']
+  },
+  {
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
       'vue/multi-word-component-names': 'off',
